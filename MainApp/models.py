d62331e8d082d557609b2bfd045bfba9db2db188
MePsyDuck <sumit231996@gmail.com>
2018-09-26 22:29:41 +0530

    Model changes

--- MainApp/models.py
+++ MainApp/models.py
@@ -2,14 +2,13 @@ from django.db import models
 
 
 class Schedule(models.Model):
-    vtu_id = models.IntegerField()
-    tts_id = models.IntegerField()
-    sub_id = models.IntegerField()
-    room_id = models.IntegerField()
-    seat_id = models.IntegerField()
+    vtu_id = models.CharField(max_length=8)
+    tts_id = models.CharField(max_length=8)
+    sub_id = models.CharField(max_length=9)
+    room_id = models.CharField(max_length=4)
     date = models.DateField()
     session = models.CharField(max_length=2, choices=[('AN', 'AN'), ('FN', 'FN')])
 
     def str(self):
-        return 'Seat: %d, Room: %d, Sub: %d, Session: %d, Date: %s'.format(self.seat_id, self.room_id, self.sub_id,
+        return 'Seat: %d, Room: %d, Sub: %d, Session: %d, Date: %s'.format(self.room_id, self.sub_id,
                                                                            self.session, str(self.date))
